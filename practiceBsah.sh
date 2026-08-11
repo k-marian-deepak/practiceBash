2f5368c74ad51372156f79233c2d44f1cac53e5d
k-marian-deepak <kmariandeepak@gmail.com>
2026-08-11 14:42:14 +0530

    Argument looping

--- practiceBsah.sh
+++ practiceBsah.sh
@@ -10,6 +10,8 @@ plus do notice how there is sapce in the begning of the single quote
 
 NAME="John Doe"
 echo -e "Hello ${NAME}, from Bash!\n"
-echo -e "Enter your name: bash practiceBash.sh \"John Doe\"\n"
-echo -e "Hello ${1}, from ${0}!\n"
+echo -e "Enter your name/names: bash practiceBash.sh \"John Doe\"\n"
+for arg in "$@"; do
+	echo -e "Hello ${arg}, from ${0}!\n"
+done
 echo -e "Total arguments passed: ${#}\n"
