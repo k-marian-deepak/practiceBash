3ad51ddf00ef23a4c59c2e206e6449bd6b6c1177
k-marian-deepak <kmariandeepak@gmail.com>
2026-08-11 14:17:09 +0530

    Basic arguments

--- practiceBsah.sh
+++ practiceBsah.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #This a single line comment block
+
 : '
 This is a multi-line comment block.
 do not use single quotes inside this
@@ -8,4 +9,7 @@ plus do notice how there is sapce in the begning of the single quote
 '
 
 NAME="John Doe"
-echo "Hello ${NAME}, from Bash!"
+echo -e "Hello ${NAME}, from Bash!\n"
+echo -e "Enter your name: bash practiceBash.sh \"John Doe\"\n"
+echo -e "Hello ${1}, from ${0}!\n"
+echo -e "Total arguments passed: ${#}\n"
