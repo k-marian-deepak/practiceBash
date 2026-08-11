831d5d0f6b5c0cc36b6c3e1c280718a9707f0bb7
k-marian-deepak <kmariandeepak@gmail.com>
2026-08-11 13:52:59 +0530

    Inline bash comment

--- practiceBsah.sh
+++ practiceBsah.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#This a single line comment block
+: '
+This is a multi-line comment block.
+don't use single quotes inside this
+plus do notice how there is sapce in the begning of the single quote
+'
+
 NAME="John Doe"
 echo "Hello ${NAME}, from Bash!"
