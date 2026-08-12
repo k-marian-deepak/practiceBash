4dce732556166d536737298109681e96c5f216a6
k-marian-deepak <kmariandeepak@gmail.com>
2026-08-12 08:44:54 +0530

    Functoin def and call

--- practiceBsah.sh
+++ practiceBsah.sh
@@ -7,11 +7,14 @@ This is a multi-line comment block.
 do not use single quotes inside this
 plus do notice how there is sapce in the begning of the single quote
 '
+get_name(){
+for arg in "$@"; do
+	echo -e "Hello ${arg}, from ${0}!\n"
+done
+}
 
 NAME="John Doe"
 echo -e "Hello ${NAME}, from Bash!\n"
 echo -e "Enter your name/names: bash practiceBash.sh \"John Doe\"\n"
-for arg in "$@"; do
-	echo -e "Hello ${arg}, from ${0}!\n"
-done
+get_name "$@"
 echo -e "Total arguments passed: ${#}\n"
