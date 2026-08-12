c8b4695bdf52ede894e43adcf70f056f2f0f769b
k-marian-deepak <kmariandeepak@gmail.com>
2026-08-12 11:54:27 +0530

    Logging the execution

--- practiceBsah.sh
+++ practiceBsah.sh
@@ -9,9 +9,16 @@ plus do notice how there is sapce in the begning of the single quote
 '
 get_name(){
 > greetings.txt
+
 for arg in "$@"; do
 	echo -e "Hello ${arg}, from ${0}!\n" >> greetings.txt
 done
+
+touch greetingLog.txt
+
+echo "-------- Logged on ---- $(date) --------" >> greetingLog.txt
+echo "" >> greetingLog.txt
+cat greetings.txt >> greetingLog.txt
 }
 
 NAME="John Doe"
@@ -21,9 +28,8 @@ echo -e "Enter your name/names: bash practiceBash.sh \"John Doe\"\n"
 if [[ -n "$1" ]]; then
 	get_name "$@"
 	cat greetings.txt
-
 else
-	echo -e "Hello ${NAME}, from ${0}!\n"
+	get_name "${NAME}"
 fi
 
 echo -e "Total arguments passed: ${#}\n"
